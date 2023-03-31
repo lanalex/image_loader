eaabf094c0fc8eada773c1d078ade360c68ac26a
AlexanderSeeDoo <alex@seedoo.co>
2023-03-31 14:16:20 +0300

    fix

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_namespace_packages, find_packages
 setup(
     name="seedoo",
     version="1.0",
+    namespace_packages=['seedoo'],
     packages=find_namespace_packages(include=['seedoo*']) + find_packages()
 )
