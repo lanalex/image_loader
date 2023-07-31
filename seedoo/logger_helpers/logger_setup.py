1d856f3164f225cb59d71c04e2b2ed2bb393262f
AlexanderSeeDoo <alex@seedoo.co>
2023-07-31 07:31:04 +0300

    add additional tests

--- seedoo/logger_helpers/logger_setup.py
+++ seedoo/logger_helpers/logger_setup.py
@@ -6,9 +6,89 @@ import platform
 
 import os
 import multiprocessing
+import torch
+import cv2
 
 num_cores = multiprocessing.cpu_count()
+
+# Setting for NumExpr
 os.environ['NUMEXPR_MAX_THREADS'] = str(num_cores)
+
+# Setting for OpenMP
+os.environ['OMP_NUM_THREADS'] = str(num_cores)
+os.environ['MKL_NUM_THREADS'] = str(num_cores)
+
+# Setting for PyTorch
+torch.set_num_threads(num_cores)
+
+# Setting for OpenCV (cv2)
+cv2.setNumThreads(num_cores)
+
+import io
+import sys
+
+def get_numpy_config():
+    import numpy as np
+    config_dict = {}
+
+    for name, info_dict in np.__config__.__dict__.items():
+        if name.startswith("_") or type(info_dict) is not dict:
+            continue
+        config_dict[name] = info_dict
+
+    # Get additional CPU features information
+    cpu_features = {
+        'baseline': ','.join(np.core._multiarray_umath.__cpu_baseline__),
+        'found': ','.join(feature for feature in np.core._multiarray_umath.__cpu_dispatch__ if np.core._multiarray_umath.__cpu_features__[feature]),
+        'not_found': ','.join(feature for feature in np.core._multiarray_umath.__cpu_dispatch__ if not np.core._multiarray_umath.__cpu_features__[feature])
+    }
+    config_dict['cpu_features'] = cpu_features
+
+    return config_dict
+
+
+def check_optimizations_and_multithreading():
+    initialize_logger()
+    import numpy as np
+    import cv2
+    import torch
+
+    logger = logging.getLogger(__name__)
+
+    # NumPy
+    np_config = get_numpy_config()
+    blas_optimization = np_config.get('blas_opt_info', {})
+    lapack_optimization = np_config.get('lapack_opt_info', {})
+
+    if (blas_optimization or lapack_optimization) and \
+            ('mkl' in str(blas_optimization) or 'openblas' in str(blas_optimization)):
+        logger.info("NumPy has both optimization and multi-threading support.")
+    else:
+        logger.critical("NumPy does not have both optimization and multi-threading support.")
+
+    # OpenCV (cv2)
+    build_info = cv2.getBuildInformation()
+
+    if ('TBB' in build_info or 'Parallel framework' in build_info) and \
+            ('OpenBLAS' in build_info or 'LAPACK' in build_info):
+        logger.critical("OpenCV has both multi-threading and optimization support.")
+    else:
+        logger.critical("OpenCV does not have both multi-threading and optimization support.")
+
+    # PyTorch
+    pytorch_optimization = torch.backends.mkl.is_available() or torch.cuda.is_available()
+    # Assuming OpenMP support for PyTorch; can't directly check
+    pytorch_multithreading = True if platform.system() != 'Darwin' else False
+
+    if platform.system() == 'Darwin' and platform.processor() == 'arm':
+        # Special handling for Apple Silicon (M1) if needed
+        pass
+
+    if pytorch_optimization and pytorch_multithreading:
+        logger.critical("PyTorch has both optimization and multi-threading support.")
+    else:
+        logger.critical("PyTorch does not have both optimization and multi-threading support.")
+
 
 def initialize_logger():
     LOGS_DIR = os.path.expanduser("~/seedoo_logs")
@@ -49,3 +129,5 @@ def initialize_logger():
         logger.addHandler(error_file_handler)
 
     return logger
+
+check_optimizations_and_multithreading()
