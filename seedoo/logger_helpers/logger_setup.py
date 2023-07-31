feb61d0c79313a83f3a9d6927966dd6b6c546ec7
AlexanderSeeDoo <alex@seedoo.co>
2023-07-31 07:15:11 +0300

    intialize logger and num thraeds

--- seedoo/logger_helpers/logger_setup.py
+++ seedoo/logger_helpers/logger_setup.py
@@ -2,10 +2,18 @@ import logging
 import logging.handlers
 import os
 from logging.handlers import SysLogHandler
+import platform
+
+import os
+import multiprocessing
+
+num_cores = multiprocessing.cpu_count()
+os.environ['NUMEXPR_MAX_THREADS'] = str(num_cores)
 
 def initialize_logger():
+    LOGS_DIR = os.path.expanduser("~/seedoo_logs")
 
-    write_to_file = bool(os.environ.get("SEEDOO_WRITE_LOGS_TO_FILE", "False"))
+    write_to_file = bool(os.environ.get("SEEDOO_WRITE_LOGS_TO_FILE", "True"))
     # Get root logger
     logger = logging.getLogger()
 
@@ -14,24 +22,27 @@ def initialize_logger():
         return logger
 
     # Create a standard log format
-    log_format = "%(asctime)s [%(levelname)s] %(name)s (%(filename)s:%(lineno)d): %(message)s"
+    log_format = "DoLoopSeeDoo: %(asctime)s [%(levelname)s] %(name)s (%(module)s.%(filename)s:%(lineno)d): %(message)s"
     formatter = logging.Formatter(log_format, datefmt="%Y-%m-%d %H:%M:%S")
 
-    # Syslog handler (logs to syslogd)
-    syslog_handler = SysLogHandler(address='/dev/log', facility=SysLogHandler.LOG_LOCAL0)
+    syslog_address = '/var/run/syslog' if platform.system() == 'Darwin' else '/dev/log'
+    syslog_handler = SysLogHandler(address=syslog_address, facility=SysLogHandler.LOG_LOCAL0)
     syslog_handler.setFormatter(formatter)
 
-    logger.setLevel(logging.ERROR) # Default level to ERROR
+    logger.setLevel(logging.INFO) # Default level to ERROR
     logger.addHandler(syslog_handler)
 
     # If the write_to_file flag is set, add file handlers
     if write_to_file:
-        log_file = "logger.log"
+        if not os.path.exists(LOGS_DIR):
+            os.mkdir(LOGS_DIR)
+
+        log_file = os.path.join(LOGS_DIR, "seedoo.log")
         file_handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=10*1024*1024, backupCount=5)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
-        error_log_file = "error_log.log"
+        error_log_file = os.path.join(LOGS_DIR, "seedoo_error.log")
         error_file_handler = logging.handlers.RotatingFileHandler(error_log_file, maxBytes=10*1024*1024, backupCount=5)
         error_file_handler.setLevel(logging.ERROR)
         error_file_handler.setFormatter(formatter)
